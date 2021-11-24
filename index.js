c09cd3a662490e46398de10585af24fe0b214214
Jorch <jorch.rl@gmail.com>
2021-11-24 00:58:47 -0600

    Finish Example 6.c

--- index.js
+++ index.js
@@ -4,77 +4,60 @@ const cors = require("cors");
 const app = express();
 const Note = require("./models/note");
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2019-05-30T17:30:31.098Z",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2019-05-30T18:39:34.091Z",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2019-05-30T19:20:14.298Z",
-//     important: true,
-//   },
-// ];
-
 //////////// MIDDLEWARE //////////////////
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
-// app.get("/", (request, response) => {
-//     console.log("You should see the front end of the app!");
-//     response.send("hey!");
-// });
 
 ////////// Notes API /////////////////////////
 
 // Get all notes
 app.get("/api/notes", (request, response) => {
   // Fetch from MongoDB
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+  Note.find({})
+    .then((notes) => {
+      response.json(notes);
+    })
+    .catch((error) => next(error));
 });
 
 // Get note by id
 app.get("/api/notes/:id", (request, response) => {
-  // const id = request.params.id;
-  // Note.find({}).then((notes) => {
-  //   // The _id field is an object; we need to compare the toString() version
-  //   const note = notes.find((n) => n.id.toString() === id);
-  //   note ? response.json(note) : response.status(404).end();
-  // });
-
   Note.findById(request.params.id)
     .then((note) => {
-      response.json(note);
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).send();
+      }
     })
-    .catch((error) => {
-      // console.log("Error fetching note", error.message);
-      response.status(404).end();
-    });
+    .catch((error) => next(error));
+});
+
+// Update a note by id
+app.put("/api/notes/:id", (request, response) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 // Delete a note by id
 app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((n) => n.id !== id);
-  console.log("deleted note ", id);
-  response.status(200).send(`deleted note ${1}`);
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 // Save a new note
@@ -94,12 +77,37 @@ app.post("/api/notes", (request, response) => {
     // MongoDB will generate the id automatically
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
-////////////////////////////
+////////// HANDLING UNKNOWN ENDPOINT //////////////
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+////////// ERROR HANDLING MIDDLEWARE //////////////
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+///////// INIT SERVER ///////////////////////
 
 const PORT = process.env.PORT || 3001;
 
