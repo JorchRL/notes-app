fcb0afde4da70c1afc004c6b0494445c3127e0a1
Jorch <jorch.rl@gmail.com>
2021-11-29 18:00:00 -0600

    Add GET all users  endpoint in users api

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,17 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
+// GET retrieve all users
+usersRouter.get("/", async (request, response, next) => {
+  try {
+    const users = await User.find({});
+    response.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST create a new user
 usersRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
@@ -15,9 +26,12 @@ usersRouter.post("/", async (request, response, next) => {
     passwordHash: passwordHash,
   });
 
-  const savedUser = await user.save();
-
-  response.json(savedUser);
+  try {
+    const savedUser = await user.save();
+    response.json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = usersRouter();
