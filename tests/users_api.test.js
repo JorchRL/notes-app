7b510a9630ff1c4cb2bbbb16694b2ecfa68f0656
Jorch <jorch.rl@gmail.com>
2021-12-13 14:09:31 -0600

    Add initial test for users api

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -3,12 +3,18 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 
-const helper = require('./test_helper')
+const helper = require("./test_helper");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
-describe("when there is initially one user in db", () => {
+describe.skip("when there are users in the db", () => {
+  // NOTE: may refactor both describe blocks into one at a later point
+  test("should retrieve all users as json with status code 200", async () => {
+    // TODO
+  });
+});
 
+describe("when there is initially one user in db", () => {
   beforeEach(async () => {
     await User.deleteMany();
     const passwordHash = await bcrypt.hash("sekret", 10);
@@ -17,52 +23,52 @@ describe("when there is initially one user in db", () => {
       passwordHash: passwordHash,
     });
 
-    await user.save()
+    await user.save();
   });
 
-  test('shoud create a new user with a fresh username', async () => {
-    const usersAtStart = helper.usersInDB()
+  test("shoud create a new user with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDB();
 
     const newUser = {
-      username = "jrl",
-      name = 'jorch',
-      password = 'katitos'
-    }
+      username: "jrl",
+      name: "jorch",
+      password: "katitos",
+    };
 
-    await api.post('/api/users')
+    await api
+      .post("/api/users")
       .send(newUser)
       .expect(200)
-      .expect('Content-Type', /application\/json/)
-      
-      const usersAtEnd = helper.usersInDB()
-      expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
-      
-      const usernames = usersAtEnd.map (u => u.username)
-      expect(usernames).toContain(newUser.username)
-  })
+      .expect("Content-Type", /application\/json/);
 
-  test('should fail with status code 400 if username already exists', async () => {
-    const usersAtStart = helper.usersInDB()
+    const usersAtEnd = await helper.usersInDB();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test("should fail with status code 400 if username already exists", async () => {
+    const usersAtStart = await helper.usersInDB();
 
     const newUser = {
-      username = "root",
-      name = 'SuperUser',
-      password = 'anotherpassword'
-    }
+      username: "root",
+      name: "SuperUser",
+      password: "anotherpassword",
+    };
 
     const result = await api
-      .post('/api/users')
+      .post("/api/users")
       .send(newUser)
       .expect(400)
-      .expect('Content-Type', /application\/json/)
-      
-      expect(result.body.error).toContain('`username` to be unique')
+      .expect("Content-Type", /application\/json/);
 
-      const usersAtEnd = helper.usersInDB()
-      expect(usersAtEnd).toHaveLength(usersAtStart.length)
-  })
-  
+    expect(result.body.error).toContain("`username` to be unique");
+    expect(result.body.errorName).toContain("ValidationError");
 
+    const usersAtEnd = await helper.usersInDB();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
 });
 
 afterAll(() => {
