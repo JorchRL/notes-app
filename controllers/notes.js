8b9b6b5fc017d795196eeb0faae99fdcc68d1138
Jorch <jorch.rl@gmail.com>
2021-12-13 14:24:04 -0600

    Modify POST api/notes/ to save the user that posted the note, according to new note and user schemas

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 /// This is our notes api handled with an express Router
 
@@ -12,15 +13,19 @@ notesRouter.get("/", async (request, response, next) => {
 // Save a new note
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   });
 
   try {
     const savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
     response.json(savedNote);
   } catch (exception) {
     next(exception);
